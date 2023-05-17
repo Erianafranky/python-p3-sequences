7d0644bb542c1685a9c0e4d634a918c96f9ad917
DorothyAriana <arianadottie@gmail.com>
2023-05-17 22:02:43 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+""" 
+The Fibonacci Sequence is a series of numbers
+The next number is found by adding up the two numbers before it
+function print_fibonacci() prints empty list when length = 0
+prints 0 when length = 1
+prints 0, 1 when length = 2
+prints 0,1,1,2,3,5,8,13,21,34 when length = 10
+ """
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(length): 
+    result = []
+    counter = 0
+    for i in range(length):
+        result.append(counter)
+        if counter == 0:
+            counter = 1
+        else:
+            counter=result[i]+result[i-1]
+        
+    print(result)
+
